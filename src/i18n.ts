20912bf759fa2ef07f340635da2399933c8b779a
bvelasqo <brandon.velasquez.osorio@gmail.com>
2024-03-12 13:28:47 -0500

    handle i18n middleware

--- src/i18n.ts
+++ src/i18n.ts
@@ -1,12 +1,13 @@
 import { notFound } from 'next/navigation';
 import { getRequestConfig } from 'next-intl/server';
 import { headers } from 'next/headers';
-import { log } from 'console';
+import { IPGeolocationAPI } from 'ip-geolocation-api-sdk-typescript';
+import { GeolocationParams } from 'ip-geolocation-api-sdk-typescript/GeolocationParams';
 
 // Can be imported from a shared config
-const locales = ['en', 'es', 'fr', 'co', 'ar', 'br', 'cl', 'mx', 'pe', 'uy', 've'];
+const locales = ['en', 'es', 'fr'];
 
-export default getRequestConfig(async () => {
+export default getRequestConfig(async ({ locale }) => {
   // Validate that the incoming `locale` parameter is valid
   const header = headers()
   let ip = header.get('x-real-ip') as string;
@@ -16,7 +17,7 @@ export default getRequestConfig(async () => {
     ip = forwardedFor?.split(",").at(0) ?? "Unknown";
   }
   const lang = await getCountryByIp(ip);
-  if (!locales.includes(lang as any)) notFound();
+  if (!locales.includes(lang)) notFound();
   console.log('lang', lang);
   return {
     messages: (await import(`../messages/${lang}.json`)).default
--- src/middleware.ts
+++ src/middleware.ts
@@ -1,24 +1,47 @@
-import { NextFetchEvent, NextRequest, NextResponse } from 'next/server';
-import createMiddleware from 'next-intl/middleware';
+import createIntlMiddleware from 'next-intl/middleware';
+import { headers } from 'next/headers';
+import { NextRequest } from 'next/server';
 
-export default createMiddleware({
-  // A list of all locales that are supported
-  locales: ['en', 'es', 'fr'],
-  defaultLocale: 'en',
-  // Used when no locale matches
-  localeDetection: false,
-  localePrefix: 'always',
-  pathnames: {
-    // The key is the pathname for the given locale
-    // The value is the pathname for the default locale
-    en: '/en',
-    es: '/es',
-    fr: '/fr'
+export default async function middleware(request: NextRequest) {
+  const header = headers()
+  let ip = header.get('x-real-ip') as string;
+
+  const forwardedFor = header.get('x-forwarded-for') as string
+  if (!ip && forwardedFor) {
+    ip = forwardedFor?.split(",").at(0) ?? "Unknown";
   }
-});
+  const lang = await getCountryByIp(ip);
+  // Step 2: Create and call the next-intl middleware (example)
+  const handleI18nRouting = createIntlMiddleware({
+    // A list of all locales that are supported
+    locales: ['en', 'es', 'fr'],
+    defaultLocale: lang,
+    // Used when no locale matches
+    localeDetection: false,
+    localePrefix: 'always',
+    pathnames: {
+      // The key is the pathname for the given locale
+      // The value is the pathname for the default locale
+      en: '/en',
+      es: '/es',
+      fr: '/fr'
+    }
+  });
+  const response = handleI18nRouting(request);
+
+  return response;
+}
+
+
 
 export const config = {
   // Match only internationalized pathnames
   matcher: ['/', '/(es|en|fr)/:path*']
 };
 
+async function getCountryByIp(ip: string) {
+  const response = await fetch(`http://ip-api.com/json/${ip}`);
+  const data = await response.json();
+  console.log('data', data);
+  return data.countryCode?.toLowerCase() ?? 'en';
+}
